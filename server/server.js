91eb64cab9fcae1deced9f05f2f7d805521119de
chayan-mann <chayanmann09@gmail.com>
2025-12-06 22:26:32 +0530

    push

---
+++ server/server.js
@@ -0,0 +1,71 @@
+import { ApolloServer } from "@apollo/server";
+import {startStandaloneServer} from '@apollo/server/standalone';
+
+const users = [
+    {id: "1", name: "John Doe", age: 23, isMarried: true},
+    {id: "2", name: "John ", age: 23, isMarried: true}
+]
+const typeDefs = `
+    type Query {
+        getUsers: [User]
+        getUserById(id: ID!): User
+    }
+
+    type Mutation {
+        createUser(
+        name: String!,
+        age: Int!,
+        isMarried: Boolean!
+        ): User
+    }
+
+    type User {
+        id: ID
+        name: String
+        age: Int
+        isMarried: Boolean
+    }
+`;
+
+const resolvers = {
+    Query: {
+        getUsers: () => {
+            return users;
+        },
+        getUserById: (parent, args) => {
+            const id = args.id
+            return users.find(user => user.id);
+        },
+    },
+    Mutation: {
+        createUser: (parent, args) => {
+            const {name, age, isMarried} = args;
+            const newUser = {
+                id: (users.length+ 1).toString(),
+                name,
+                age,
+                isMarried,
+            };
+            users.push(newUser);
+        }
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs, resolvers
+});
+
+const {url} = await startStandaloneServer(server, {
+    listen : { port:4000},
+});
+
+console.log(`Server running at: ${url}`);
+
+
+// query, mutation 
+
+
+
+
+
+
